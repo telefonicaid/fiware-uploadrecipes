3006d95a917673d12ca9ddb1cb88953422c2abac
Henar Munoz <henar@tid.es>
2014-04-15 07:56:15 +0200

    files for jenkins

---
+++ build.sh
@@ -0,0 +1,16 @@
+# File to execute the covertura and unit test and generate the information
+# 
+to be shown in sonar
+#
+# __author__ = 'henar'
+
+
+virtualenv ENV
+source 
+ENV/bin/activate
+pip install -r requirements.txt
+
+python manage.py syncdb
+
+python manage.py test --settings=settings.jenkins deactivate
+
